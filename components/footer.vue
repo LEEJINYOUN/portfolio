032ab587ef12c74d0ab48ad6a4a290183bb4c44b
LEE JINYOUNG <swlsdud1005@naver.com>
2024-05-13 12:40:14 +0900

    컨텍트페이지 수정

---
+++ components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    <div class="footer_bg">
+      <div class="footer_container container">
+        <div class="main_footer">
+          <div class="footer_social">
+            <a href="https://github.com/LEEJINYOUN">
+              <img
+                src="https://swlsdud1005.sirv.com/portfolio/icon/github_icon.svg"
+                alt=""
+            /></a>
+          </div>
+          <div class="footer_copy">
+            &#169; 2024. Lee Jin Young. All right reserved
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -4,5 +4,25 @@ definePageMeta({
 });
 </script>
 <template>
-  <div>contact</div>
+  <section class="contact section" id="contact">
+    <h2 class="section_title">- Contact me -</h2>
+    <div class="contact_container container">
+      <div class="contact_content">
+        <div class="inputs">
+          <input type="text" id="name" required placeholder="이름" />
+          <input type="email" id="email" required placeholder="이메일" />
+        </div>
+        <input type="text" id="phone" required placeholder="전화번호" />
+        <textarea
+          id="message"
+          cols="30"
+          rows="10"
+          required
+          placeholder="메세지"
+        ></textarea>
+        <button class="btn">보내기</button>
+      </div>
+    </div>
+  </section>
+  <Footer />
 </template>
