1051c49be821fa849f326343a885fbd3e28af5c1
LEE JINYOUNG <swlsdud1005@naver.com>
2024-05-13 12:12:27 +0900

    메인페이지 수정

--- pages/index.vue
+++ pages/index.vue
@@ -5,17 +5,13 @@ definePageMeta({
 </script>
 
 <template>
-  <section class="relative" id="home">
-    <div
-      class="home_container bg-[url('./images/home-bg.jpg')] bg-no-repeat bg-cover bg-bottom relative z-10 h-screen"
-    >
-      <div
-        class="home_content absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] m-auto px-10 py-5 max-w-[1000px] w-5/6 justify-start"
-      >
-        <h1 class="">안녕하세요, <span></span></h1>
+  <section class="home" id="home">
+    <div class="home_container">
+      <div class="home_content">
+        <h1>안녕하세요, <span></span></h1>
         <h1>신입 프론트엔드 개발자 <span></span></h1>
         <h1>이진영입니다. <span></span></h1>
-        <a href="#resume" class="btn"> 더 알아보기 <span></span></a>
+        <NuxtLink to="/resume" class="btn"> 더 알아보기 <span></span></NuxtLink>
       </div>
     </div>
   </section>
