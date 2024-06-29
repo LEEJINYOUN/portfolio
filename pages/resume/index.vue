f7394334663f7e175e08acf5045035f4b63288ce
LEE JINYOUNG <swlsdud1005@naver.com>
2024-06-29 10:38:44 +0900

    컴포넌트 정리

--- pages/resume/index.vue
+++ pages/resume/index.vue
@@ -7,7 +7,198 @@ definePageMeta({
   <LazyLayoutSectionLayout>
     <LazyLayoutContainerLayout>
       <LazyTextSectionTitle>- timeline -</LazyTextSectionTitle>
-      <LazyContainerTimeline />
+      <div class="w-full max-w-3xl mx-auto">
+        <div
+          class="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:mx-auto md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent"
+        >
+          <div class="flex flex-col gap-12">
+            <div
+              class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active"
+            >
+              <div
+                class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
+              >
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="10"
+                >
+                  <path
+                    fill-rule="nonzero"
+                    d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+                  />
+                </svg>
+              </div>
+              <div
+                class="w-[calc(100%-4rem)] md:w-[50%] bg-white p-4 rounded border border-slate-200 shadow"
+              >
+                <div class="flex items-center justify-between space-x-2 mb-1">
+                  <div class="font-bold text-slate-900">영진전문대 입학</div>
+                  <time class="font-caveat font-medium text-indigo-500"
+                    >2013.03</time
+                  >
+                </div>
+                <div class="text-slate-500">전자 정보통신학과</div>
+              </div>
+            </div>
+            <div
+              class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active"
+            >
+              <div
+                class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
+              >
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="10"
+                >
+                  <path
+                    fill-rule="nonzero"
+                    d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+                  />
+                </svg>
+              </div>
+              <div
+                class="w-[calc(100%-4rem)] md:w-[50%] bg-white p-4 rounded border border-slate-200 shadow"
+              >
+                <div class="flex items-center justify-between space-x-2 mb-1">
+                  <div class="font-bold text-slate-900">군 입대</div>
+                  <time class="font-caveat font-medium text-indigo-500"
+                    >2013.10 ~ 2015.07</time
+                  >
+                </div>
+                <div class="text-slate-500">육군병장 만기전역</div>
+              </div>
+            </div>
+            <div
+              class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active"
+            >
+              <div
+                class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
+              >
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="10"
+                >
+                  <path
+                    fill-rule="nonzero"
+                    d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+                  />
+                </svg>
+              </div>
+              <div
+                class="w-[calc(100%-4rem)] md:w-[50%] bg-white p-4 rounded border border-slate-200 shadow"
+              >
+                <div class="flex items-center justify-between space-x-2 mb-1">
+                  <div class="font-bold text-slate-900">영진전문대 졸업</div>
+                  <time class="font-caveat font-medium text-indigo-500"
+                    >2018.02</time
+                  >
+                </div>
+                <div class="text-slate-500"></div>
+              </div>
+            </div>
+            <div
+              class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active"
+            >
+              <div
+                class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
+              >
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="10"
+                >
+                  <path
+                    fill-rule="nonzero"
+                    d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+                  />
+                </svg>
+              </div>
+              <div
+                class="w-[calc(100%-4rem)] md:w-[50%] bg-white p-4 rounded border border-slate-200 shadow"
+              >
+                <div class="flex items-center justify-between space-x-2 mb-1">
+                  <div class="font-bold text-slate-900">
+                    리쿠르트 R&D 스태핑
+                  </div>
+                  <time class="font-caveat font-medium text-indigo-500"
+                    >2018.03 ~ 2020.09</time
+                  >
+                </div>
+                <div class="text-slate-500">
+                  일본 IT 기업 리쿠르트 R&D 스태핑 정사원 (기술직 파견사원)
+                </div>
+              </div>
+            </div>
+            <div
+              class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active"
+            >
+              <div
+                class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
+              >
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="10"
+                >
+                  <path
+                    fill-rule="nonzero"
+                    d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+                  />
+                </svg>
+              </div>
+              <div
+                class="w-[calc(100%-4rem)] md:w-[50%] bg-white p-4 rounded border border-slate-200 shadow"
+              >
+                <div class="flex items-center justify-between space-x-2 mb-1">
+                  <div class="font-bold text-slate-900">웹 개발 공부</div>
+                  <time class="font-caveat font-medium text-indigo-500"
+                    >2022.04 ~
+                  </time>
+                </div>
+                <div class="text-slate-500"></div>
+              </div>
+            </div>
+            <div
+              class="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group is-active"
+            >
+              <div
+                class="flex items-center justify-center w-10 h-10 rounded-full border border-white bg-slate-300 group-[.is-active]:bg-emerald-500 text-slate-500 group-[.is-active]:text-emerald-50 shadow shrink-0 md:order-1 md:group-odd:-translate-x-1/2 md:group-even:translate-x-1/2"
+              >
+                <svg
+                  class="fill-current"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="12"
+                  height="10"
+                >
+                  <path
+                    fill-rule="nonzero"
+                    d="M10.422 1.257 4.655 7.025 2.553 4.923A.916.916 0 0 0 1.257 6.22l2.75 2.75a.916.916 0 0 0 1.296 0l6.415-6.416a.916.916 0 0 0-1.296-1.296Z"
+                  />
+                </svg>
+              </div>
+              <div
+                class="w-[calc(100%-4rem)] md:w-[50%] bg-white p-4 rounded border border-slate-200 shadow"
+              >
+                <div class="flex items-center justify-between space-x-2 mb-1">
+                  <div class="font-bold text-slate-900">민트소프트</div>
+                  <time class="font-caveat font-medium text-indigo-500"
+                    >2024.01 ~ 2024.05</time
+                  >
+                </div>
+                <div class="text-slate-500">프론트엔드 개발자 정사원</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </LazyLayoutContainerLayout>
     <LazyLayoutContainerLayout>
       <LazyTextSectionTitle>- skills -</LazyTextSectionTitle>
